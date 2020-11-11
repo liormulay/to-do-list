b6d1ac458037838a7a56a7f2e1257e5cbdf14c7e
shir shukrun <shirshukrun1@gmail.com>
2020-11-11 20:51:42 +0200

    save method

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,17 +1,20 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entities.ToDoList;
+import com.example.demo.entities.ToDoListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
 
-//    @GetMapping(value = "/")
-//    @RequestMapping(value="/",method= RequestMethod.GET)
-    public String hello() {
-        return "index.html";
-    }
+    @Autowired
+    ToDoListRepository toDoListRepository;
 
+    @RequestMapping(value = "/api/list", method = RequestMethod.POST)
+    public ResponseEntity<?> saveList(@RequestBody ToDoList toDoList) {
+        toDoListRepository.save(toDoList);
+        return ResponseEntity.ok().build();
+    }
 }
