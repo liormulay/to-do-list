7ba0aba70dc105d1871b7cc2be881e16c5f81f45
liorm <liormualy6@gmail.com>
2020-11-27 14:51:40 +0200

    add buttons to container

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,17 @@ var MainMenu = function () {
         document.getElementById("addNoteBtn").addEventListener("click", onAddClick);
         axios.get('/api/lists')
         .then((response) => {
-            console.log(response.data);
+            var data = response.data;
+            const container = document.getElementById("gridNotes");
+            for (let i = 0; i < data.length; i++) {
+                var date = (data[i]).dateTime;
+                const button = document.createElement("button");
+                button.innerText = date;
+                button.addEventListener("click", function () {
+                    console.log(i);
+                })
+                container.appendChild(button);
+            }
         });
     };
 
